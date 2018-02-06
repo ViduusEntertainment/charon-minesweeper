b540352c384b27a165f56d85b238e36455015f62
Ethan Toney <ethan.toney@uky.edu>
2018-02-05 23:23:27 -0500

    base project structure

---
+++ src/org/viduus/charon/minesweeper/GameRunner.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright 2018, Viduus Entertainment LLC, All rights reserved.
+ * 
+ * Created on Feb 5, 2018 by Ethan Toney
+ */
+package org.viduus.charon.minesweeper;
+
+/**
+ * @author ethan
+ *
+ */
+public class GameRunner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
